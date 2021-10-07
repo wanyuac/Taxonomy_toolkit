e23c27aa34e89cbd7f546559f70f9f21e9f09012
wanyuac <wanyuac@126.com>
2021-10-08 00:14:14 +0100

    Add copyright information

--- compile_bracken_species_abundance.sh
+++ compile_bracken_species_abundance.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# Yu Wan (17/5/2021)
+# Extract the line corresponding to a particular taxon from the tsv-formatted bracken reports.
 # Example command:
-#    sh compile_bracken_species_abundance.sh [species name] [report directory (no backslash)] [genome_list.txt] > output.tsv
+#    sh compile_bracken_species_abundance.sh [taxon name] [report directory (no backslash)] [genome_list.txt] > output.tsv
+# Copyright (C) 2021 Yu Wan <wanyuac@126.com>
+# Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
+# Creation: 17 May 2021; the latest update: 17 May 2021
 
 echo -e 'isolate\tspecies\ttaxonomy_id\ttaxonomy_lvl\tkraken_assigned_reads\tadded_reads\tnew_est_reads\tfraction_total_reads'
 species=$1
